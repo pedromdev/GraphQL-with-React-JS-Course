b2ff868f0b53e643a65056e0edbdaa243c9b613f
Pedro Alves <pedromarcelodesaalves@gmail.com>
2019-02-04 22:25:03 -0300

    A client-server GraphQL example to used throughout the course

---
+++ Lyrical-GraphQL/client/components/SongCreate.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class SongCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: ''
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h3>Create a new song</h3>
+        <form>
+          <label>Song title:</label>
+          <input
+            onChange={event => this.setState({ title: event.target.value })} 
+            value={this.state.title}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SongCreate;
